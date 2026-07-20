51c4012cd402bb3dd1ad8f04fc7a89a58bef9c07
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-07-20 23:04:26 +0600

    load and display all countries data from API by fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './component/countries/Countries'
+
+const countriesPromise = fetch ('https://openapi.programming-hero.com/api/all')
+.then (res => res.json ())
 
 function App() {
 
   return (
         <div>
-          <Countries></Countries>
+          <Suspense fallback = {<p>Nadir Vai is Loading...</p>}>
+            <Countries countriesPromise = {countriesPromise}></Countries>
+          </Suspense>
         </div>
   )
 }
---
+++ src/component/country/Country.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Country = ({country}) => {
+    console.log(country);
+    
+    return (
+        <div style={{margin : '20px'}}>
+              <img src = {country.flags.flags.png} alt={country.flags.flags.alt} />
+             <h3>Name : {country.name.common}</h3>
+             <h3>Population : {country.population.population}</h3>
+        </div>
+    );
+};
+
+export default Country;
+
