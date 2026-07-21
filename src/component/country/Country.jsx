d56ba3499738ced77755bd23701ae0235e36c5f7
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-07-21 23:11:44 +0600

    added conditional text using event and react state

--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css';
 
 const Country = ({country}) => {
     console.log(country);
+
+    const [state, setState] = useState ('');
+
+    const handleState = () => {
+        const updateState = 'Visited';
+        setState (updateState);
+        
+    }
     
     return (
-        <div className='country'>
+        <div className = {country.area.area > 10000 ? "country" : "country2"}>
               <img src = {country.flags.flags.png} alt={country.flags.flags.alt} />
               <h3>Name : {country.name.common}</h3>
               <h3>Population : {country.population.population}</h3>
+              <p>Area : {country.area.area} - {country.area.area > 10000 ? 'Big Country' : 'Small Country'}</p>
+              <button onClick={handleState}> {!state ? 'Not Visited' : 'Visited'} </button>
         </div>
     );
 };
