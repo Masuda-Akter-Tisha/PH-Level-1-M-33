b5cfdc8e472b32724fcf1099cf39fdb7959f2f10
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-07-21 09:59:51 +0600

    use style in react

--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './country.css';
 
 const Country = ({country}) => {
     console.log(country);
     
     return (
-        <div style={{margin : '20px'}}>
+        <div className='country'>
               <img src = {country.flags.flags.png} alt={country.flags.flags.alt} />
-             <h3>Name : {country.name.common}</h3>
-             <h3>Population : {country.population.population}</h3>
+              <h3>Name : {country.name.common}</h3>
+              <h3>Population : {country.population.population}</h3>
         </div>
     );
 };
