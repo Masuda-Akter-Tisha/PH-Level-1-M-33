09107eaa301f815e917287aeed992218ed8b070f
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-07-22 23:32:11 +0600

    explore conditional

--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -4,21 +4,32 @@ import './country.css';
 const Country = ({country}) => {
     console.log(country);
 
-    const [state, setState] = useState ('');
+    const [visited, setVisited] = useState (false);
 
-    const handleState = () => {
-        const updateState = 'Visited';
-        setState (updateState);
+    const handleVisited = () => {
+        // 1.
+        // if (visited) {
+        //     setVisited (false);
+        // }
+        // else {
+        //     setVisited (true);
+        // }
+
+        //  2. 
+        // setVisited (visited ? false : true);
+        setVisited (!visited);
+
+        console.log(country.name.common);
         
     }
     
     return (
-        <div className = {country.area.area > 10000 ? "country" : "country2"}>
+        <div className = {visited ? "country-visited" : "country"}>
               <img src = {country.flags.flags.png} alt={country.flags.flags.alt} />
               <h3>Name : {country.name.common}</h3>
               <h3>Population : {country.population.population}</h3>
               <p>Area : {country.area.area} - {country.area.area > 10000 ? 'Big Country' : 'Small Country'}</p>
-              <button onClick={handleState}> {!state ? 'Not Visited' : 'Visited'} </button>
+              <button onClick={handleVisited}> {visited ? 'Visited' : 'Not Visited'} </button>
         </div>
     );
 };
